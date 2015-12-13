1bfb12fb57aeacffde01b8a01abca018b04b1ea1
ChrisLangel <cmlangel@ucdavis.edu>
2015-12-13 13:53:44 -0800

    update naming convention

--- cleverTapLogin.js
+++ cleverTapLogin.js
@@ -1,7 +1,7 @@
 /*global cordova, module*/
 
 module.exports = {
-    sendInfo: function (email,CTid, successCallback, errorCallback) {
+    sendInfo: function (email, CTid, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "CleverTapLogin", "profileInfo", [email,CTid]);
     }
 };
