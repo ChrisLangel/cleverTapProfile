c8c30df803b0bb2c2e7d4849595562a4fb693aa3
ChrisLangel <cmlangel@ucdavis.edu>
2015-12-13 13:47:37 -0800

    Main content

---
+++ cleverTapLogin.js
@@ -0,0 +1,7 @@
+/*global cordova, module*/
+
+module.exports = {
+    sendInfo: function (email,CTid, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "CleverTapLogin", "profileInfo", [email,CTid]);
+    }
+};
---
+++ ios/CleverTapLoginiOS.h
@@ -0,0 +1,7 @@
+#import <Cordova/CDV.h>
+
+@interface CleverTapLoginiOS : CDVPlugin
+
+- (void) profileInfo:(CDVInvokedUrlCommand*)command;
+
+@end
