575139bcecbeb3b1df0de4ef835949fb308ec17c
ChrisLangel <cmlangel@ucdavis.edu>
2015-12-13 18:23:10 -0800

    no longer calling callback

--- src/ios/CleverTapLoginiOS.m
+++ src/ios/CleverTapLoginiOS.m
@@ -6,11 +6,9 @@
 - (void)profileInfo:(CDVInvokedUrlCommand*)command
 {
 
-    NSString* callbackId = [command callbackId];
     NSString* name  = [[command arguments] objectAtIndex:0];
     NSString* email = [[command arguments] objectAtIndex:1];
     NSString* ctId  = [[command arguments] objectAtIndex:2];
-    // NSString* msg = [NSString stringWithFormat: @"Hello, %@, %@", email,ctId];
 
     NSDictionary *profile = @{@"Name"     : name,
                               @"Email"    : email, // String
@@ -18,12 +16,6 @@
 
     [[CleverTap push] profile:profile];
 
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    // CDVPluginResult* result = [CDVPluginResult
-    //                            resultWithStatus:CDVCommandStatus_OK
-    //                            messageAsString:msg];
-
-    [self success:pluginResult callbackId:callbackId];
 }
 
 @end
